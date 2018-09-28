873b6b47774ae00232f6f7ea815a7eab7cda66b8
David Sim <wsim903@gmail.com>
2018-09-28 20:41:02 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,11 +4,7 @@ end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  {"whale bone corsets" => , "oil paintings" => , "porcelain vases" => }
 end
 
 def retrieval
